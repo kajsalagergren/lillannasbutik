3d5a62c479fe9b822b8fcd14b9fb37f84c1ca180
kajsalagergren <kajsalagergren96@gmail.com>
2026-04-20 13:04:38 +0200

    Laddar upp Lillannasbutik

---
+++ js/script.js
@@ -0,0 +1,83 @@
+
+/* Galleri           */
+const images = [
+    {src: 'images/Finaform.jpg', alt: 'Keramik från Finaform' },
+    {src: 'images/Fruktträd.jpg', alt: 'Fruktträd för plantering' },
+    {src: 'images/Plåtrosor.jpg', alt: 'Plåtrosor gjorda av Emelie' },
+    {src: 'images/Lyktor.png', alt: 'Olika typer av lyktor'},
+    {src: 'images/Ljus.png', alt: 'Polckarandiga ljus'}
+];
+
+const galleryContainer = document.getElementById('gallery-container');
+let currentImageIndex = 0; // Håller koll på vilken bild som är öppen
+
+function displayImages() {
+    if (!galleryContainer) return;
+    galleryContainer.innerHTML = "";
+
+    images.forEach(image => {
+        const imageCard = `
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="gallery-item" onclick="openLightbox('${image.src}')">
+                    <img src="${image.src}" alt="${image.alt}" class="img-fluid rounded shadow-sm">
+                </div>
+            </div>
+        `;
+        galleryContainer.innerHTML += imageCard;
+    });
+}
+
+// Öppnar modalen och hittar rätt start-index
+function openLightbox(src) {
+    currentImageIndex = images.findIndex(img => img.src === src);
+    updateModalContent();
+
+    const modalElement = document.getElementById('galleryModal');
+    const myModal = bootstrap.Modal.getOrCreateInstance(modalElement);
+    myModal.show();
+}
+
+// Byter bild (direction är 1 eller -1)
+function changeImage(direction) {
+    currentImageIndex += direction;
+
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0; // Gå till början
+    } else if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1; // Gå till slutet
+    }
+
+    updateModalContent();
+}
+
+// Uppdaterar bilden inuti modalen
+function updateModalContent() {
+    const modalImage = document.getElementById('modalImage');
+    if (modalImage) {
+        modalImage.src = images[currentImageIndex].src;
+        modalImage.alt = images[currentImageIndex].alt;
+    }
+}
+
+if (galleryContainer) {
+    displayImages();
+}
+
+/* Karta         */
+const mapContainer = document.getElementById('map');
+if (mapContainer) {
+    // ... (behåll din nuvarande kart-kod här, den ser bra ut!)
+    const lat = 57.984474;
+    const lng = 12.740857;
+    const map = L.map('map').setView([lat, lng], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+    const marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup("<b>Lillannas Butik</b><br>Välkommen in!").openPopup();
+    
+    mapContainer.addEventListener('click', function() {
+        window.open(`https://www.google.com/maps?q=${lat},${lng}`, '_blank');
+    });
+    setTimeout(() => { map.invalidateSize(); }, 200);
+}
